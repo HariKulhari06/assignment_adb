a0c8a9ccd7fa03b124702d772401416af3106c91
Hari Choudhary <harikulhari06@gmail.com>
2021-11-29 11:37:17 +0530

    Added splash screen

---
+++ app/src/main/java/com/example/assignment/ui/info/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.assignment.ui.info.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.assignment.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    private val homeViewModel: HomeViewModel by viewModels()
+
+    private var _binding: HomeFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/assignment/ui/info/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.assignment.ui.info.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
